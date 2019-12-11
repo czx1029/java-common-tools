0ee474623d4d639e7b3a530b87c1e999da60076d
czx <412359020@qq.com>
2019-12-11 16:19:53 +0800

    integerequals

--- src/main/java/com/zeroten/common/util/CheckUtils.java
+++ src/main/java/com/zeroten/common/util/CheckUtils.java
@@ -13,4 +13,26 @@ public class CheckUtils {
         }
         return true;
     }
+
+    public static boolean equals(String str1, String str2) {
+        System.out.println("str1 "+str1);
+        System.out.println("str2 "+str2);
+        if (str1==str2){
+            if (str1!=""){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean intequals(Integer n1, Integer n2) {
+        System.out.println("n1 "+ n1);
+        System.out.println("n2 "+ n2);
+        if (n1==n2){
+            if (n1!=null){
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/test/java/com/zeroten/common/util/CheckUtilsTest.java
+++ src/test/java/com/zeroten/common/util/CheckUtilsTest.java
@@ -22,4 +22,26 @@ public class CheckUtilsTest {
 //      判断引用类型数组是否为空，为空或 null 则返回 true，否则返回 false。
 
     }
+
+
+    @Test
+    public void testequals(){
+//        判断 str1 字符串是否相等，相等则返回 true，否则返回 false。当其中一个是 null 时返回 false。
+        Assert.assertEquals(CheckUtils.equals("qwqwqw","qwqwqw"),true);
+        Assert.assertEquals(CheckUtils.equals("qwqwqw","sdfsdfsdf"),false);
+        Assert.assertEquals(CheckUtils.equals("","sdfsdfsdf"),false);
+        Assert.assertEquals(CheckUtils.equals("qwqwqw",""),false);
+        Assert.assertEquals(CheckUtils.equals("",""),false);
+
+    }
+
+    @Test
+    public void testintequals(){
+//        判断 n1 和 n2 的值是否相等，相等则返回 true，否则返回 false。当其中一个是 null 时返回 false。
+        Assert.assertEquals(CheckUtils.intequals(1,1),true);
+        Assert.assertEquals(CheckUtils.intequals(1,2),false);
+        Assert.assertEquals(CheckUtils.intequals(null,1),false);
+        Assert.assertEquals(CheckUtils.intequals(1,null),false);
+        Assert.assertEquals(CheckUtils.intequals(null,null),false);
+    }
 }
